b126050d88284e33be40e8af717fe4abebf3a10b
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-04-16 16:07:08 +0500

    Create A1.dart

---
+++ A1.dart
@@ -0,0 +1,10 @@
+/*
+Question A1:
+Write a program that takes as input the command line arguments of types string, integer and decimal value. In case of a string, its length should be displayed, for an integer value, it should be multiplied by 100, and for a decimal value, we need to take its power of 3.
+*/
+
+void main(List<String> arguments) {
+for(var arg in arguments) {
+print(arg);
+}
+}
