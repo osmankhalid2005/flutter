a20e1fb150d76f1e3bef3515ac1455366efd8b0b
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-05-12 15:45:08 +0500

    Update A1.dart

--- problem_set_solutions/A1.dart
+++ problem_set_solutions/A1.dart
@@ -3,10 +3,23 @@ Question A1:
 Write a program that takes as input the command line arguments of types string, integer and decimal value. In case of a string, its length should be displayed, for an integer value, it should be multiplied by 100, and for a decimal value, we need to take its power of 3.
 */
 
-void main(List<String> arguments) {
-// this is not a complete solution.
-for(var arg in arguments) {
-print(arg);
-}
+import 'dart:math';
+
+void main(List <String>args) {
+
+for(var item in args) {
+  if(int.tryParse(item) != null) {
+    var num = int.parse(item);
+    print(num * 100);
+  }
+  else if(double.tryParse(item) != null) {
+    var num = double.parse(item);
+    print( pow(num, 3));
+  }
+  else {
+    print(item.length);
+  }
   
+}
+
 }
