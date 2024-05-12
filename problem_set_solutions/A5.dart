9ca4014844ad0fccd977defb6edb77861c4b2459
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-05-12 16:04:04 +0500

    Create A5.dart

---
+++ problem_set_solutions/A5.dart
@@ -0,0 +1,14 @@
+/*
+Suppose we have two numbers a=10 and b=20. You need to swap the numbers without using any third temporary variable, or any arithmetic or logical operators.
+*/
+
+void main(List <String>args) {
+int a = 10;
+int b = 20;
+
+(a, b) = (b, a );
+
+print(a);
+print(b);
+
+}
