af274a4076e8aa23bf4ecafae7872f16793547e3
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-07-19 15:51:42 +0500

    Update A21.dart

--- problem_set_solutions/A21.dart
+++ problem_set_solutions/A21.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomeScreen()));
+  runApp(MaterialApp(home: HomeScreen()));
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   final TextEditingController _city = TextEditingController(text: "Abbottabad");
   final TextEditingController _country =
       TextEditingController(text: "Pakistan");
+
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
