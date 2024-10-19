1712f2f4453c4b00c55ce23b2ee182ca5feb1355
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-10-19 16:38:56 +0500

    Create P56.dart

---
+++ problem_set_solutions/P56.dart
@@ -0,0 +1,30 @@
+void main() {
+  int day = 3;
+
+  // Use a switch statement to print the corresponding day
+  switch (day) {
+    case 1:
+      print('Monday');
+      break;
+    case 2:
+      print('Tuesday');
+      break;
+    case 3:
+      print('Wednesday');
+      break;
+    case 4:
+      print('Thursday');
+      break;
+    case 5:
+      print('Friday');
+      break;
+    case 6:
+      print('Saturday');
+      break;
+    case 7:
+      print('Sunday');
+      break;
+    default:
+      print('Invalid day');
+  }
+}
