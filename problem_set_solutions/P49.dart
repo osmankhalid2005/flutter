35090ba2ff48745465834c5b6e121a2af0db31c0
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-10-19 16:19:29 +0500

    Create P49.dart

---
+++ problem_set_solutions/P49.dart
@@ -0,0 +1,9 @@
+void main() {
+  // List of integers
+  List<int> numbers = [10, 20, 30, 40, 50];
+
+  // Iterate over the list and print the square of each number
+  for (int number in numbers) {
+    print('The square of $number is ${number * number}');
+  }
+}
