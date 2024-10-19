63f882291df0b1c9a7eae27d6152c4c1e582b509
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-10-19 12:09:01 +0500

    Create P3.dart

---
+++ problem_set_solutions/P3.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Taking a number as input
+
+  int number = 23;
+
+  // Checking if the number is even or odd
+  if (number % 2 == 0) {
+    print('$number is even.');
+  } else {
+    print('$number is odd.');
+  }
+}
