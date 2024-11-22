bb49cc79cd6988c653e0113a55213fbbf197e9d0
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-11-22 09:32:29 +0500

    Update P33.dart

--- problem_set_solutions/P33.dart
+++ problem_set_solutions/P33.dart
@@ -1,1 +1,19 @@
+//list by age in ascending order and prints the sorted list.
+
+void main() {
+  Map<String, int> inventory = {'Apples': 50, 'Oranges': 30, 'Bananas': 20};
+
+  /**
+  * 
+   - Update the quantity of 'Oranges' to 45.
+ - Add a new item 'Mangoes' with a quantity of 60.
+ - Remove 'Bananas' from the map.
+  */
+
+  inventory['Oranges'] = 45;
+  inventory['Mangoes'] = 60;
+  inventory.remove('Bananas');
+
+  print(inventory);
+}
 
