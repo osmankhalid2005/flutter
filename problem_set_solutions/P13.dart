c8824996ddb1e8342ea31a0024ffb0fb557500f2
osmankhalid2005 <44121080+osmankhalid2005@users.noreply.github.com>
2024-11-22 09:28:43 +0500

    Update P13.dart

--- problem_set_solutions/P13.dart
+++ problem_set_solutions/P13.dart
@@ -1,1 +1,14 @@
+//list by age in ascending order and prints the sorted list.
+
+void main() {
+  var records = [
+    (name: "Alice", age: 25),
+    (name: "Bob", age: 30),
+    (name: "Charlie", age: 22)
+  ];
+
+  records.sort((a, b) => a.age.compareTo(b.age));
+
+  print(records);
+}
 
